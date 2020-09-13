d7dac8c2b09085e0cae42cb594aebcfa3d942c38
aj-bhagat <ajaybhagatm620@gmail.com>
2020-09-13 23:54:24 +0530

    Sidebar Component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,7 @@ import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import {SearchOutlined} from "@material-ui/icons";
+import SidebarChat from './SidebarChat';
 
 function Sidebar(){
     return(
@@ -31,7 +32,10 @@ function Sidebar(){
              </div>
             </div> 
             <div className="sidebar_chats">
-
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
             </div>
         </div>
     )

cdee561f7c4672d6b79880b87a812c834e779a4a
aj-bhagat <ajaybhagatm620@gmail.com>
2020-09-15 01:36:12 +0530

    Chat Comp

--- src/Chat.js
+++ src/Chat.js
@@ -1,20 +1,52 @@
-import { Avatar } from '@material-ui/core';
-import React from 'react'
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon, Mic } from '@material-ui/icons';
+import React, { useEffect, useState }  from 'react'
 import "./Chat.css";
 
 function Chat() {
     const [seed, setSeed] = useState("");
+    const [input, setInput] = useState("");
+
+    useEffect(()=>{
+        setSeed(Math.floor(Math.random() * 5000));
+    },[]);
+
+    const sendMessage =(e) =>{
+      e.preventDefault();
+      console.log("You typed >>>",input);
+      setInput("");
+    };
+
     return (
         <div className="chat">
             <div className="chat_header">
-                <Avatar/>
-
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                <div className="chat_headerInfo">
+                    <h3>RoomName</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className="chat_headerRight">
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
             </div>
             <div className="chat_body">
-
+                <p className={`chat_message ${true &&`chat_receiver`}`}><span className="chat_name">Ajay Bhagat</span>Hey Guys<span className="chat_timestamp">12:57pm</span></p>
             </div>
             <div className="chat_footer">
-
+                <InsertEmoticon/>
+                <form>
+                  <input value={input} onChange={e=>setInput(e.target.value)} placeholder="Type a message" type="text"/>
+                  <button onClick={sendMessage} type="submit">Send a message</button>
+                </form>
+                <Mic/>
             </div>
         </div>
     )

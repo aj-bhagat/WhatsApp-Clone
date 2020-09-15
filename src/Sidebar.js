e52a74670e3c1101d1e742136e38858d6604b10e
aj-bhagat <ajaybhagatm620@gmail.com>
2020-09-16 01:14:54 +0530

    Sidebar Comp

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Sidebar.css';
 import {Avatar, IconButton} from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
@@ -6,8 +6,21 @@ import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import {SearchOutlined} from "@material-ui/icons";
 import SidebarChat from './SidebarChat';
+import db from './firebase';
 
 function Sidebar(){
+const [rooms, setRooms] = useState([]);
+
+useEffect( () => {
+    db.collection("Rooms").onSnapshot((snapshot) => setRooms(
+          snapshot.docs.map((doc)=>({
+              id: doc.id, 
+              data: doc.data() 
+            }))
+        )
+    );
+}, []);
+
     return(
         <div className="sidebar">
            <div className="sidebar_header">
@@ -33,9 +46,7 @@ function Sidebar(){
             </div> 
             <div className="sidebar_chats">
                 <SidebarChat addNewChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
+                 {rooms.map(room => (<SidebarChat key={room.id} id={room.id} name={room.data.Name}/>))}
             </div>
         </div>
     )

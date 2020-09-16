fc2a20701ade4a48ad32f273fe5427019528d2a7
aj-bhagat <ajaybhagatm620@gmail.com>
2020-09-17 02:10:28 +0530

    Chat Comp

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,28 @@
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon, Mic } from '@material-ui/icons';
 import React, { useEffect, useState }  from 'react'
+import { useParams } from 'react-router-dom';
 import "./Chat.css";
+import db from './firebase';
 
 function Chat() {
     const [seed, setSeed] = useState("");
     const [input, setInput] = useState("");
+    const {roomId} = useParams();
+    const [roomName, setRoomName] = useState("");
+
+    useEffect( () => {
+        if(roomId){
+            db.collection("Rooms").doc(roomId)
+            .onSnapshot(snapshot => (
+                setRoomName(snapshot.data().Name)
+            ))
+        }
+    },[roomId])
 
     useEffect(()=>{
         setSeed(Math.floor(Math.random() * 5000));
-    },[]);
+    },[roomId]);
 
     const sendMessage =(e) =>{
       e.preventDefault();
@@ -22,7 +35,7 @@ function Chat() {
             <div className="chat_header">
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
                 <div className="chat_headerInfo">
-                    <h3>RoomName</h3>
+                    <h3>{roomName}</h3>
                     <p>Last seen at...</p>
                 </div>
                 <div className="chat_headerRight">

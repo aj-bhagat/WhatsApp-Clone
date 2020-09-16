aac95acc002caa999896188b6eaf8d78308ba3f8
aj-bhagat <ajaybhagatm620@gmail.com>
2020-09-17 02:08:45 +0530

    Login Comp

---
+++ src/Login.js
@@ -0,0 +1,28 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import { auth, provider } from './firebase';
+import "./Login.css";
+
+function Login() {
+    const signIn = () => {
+        auth
+        .signInWithPopup(provider)
+        .then((result) => console.log(result))
+        .catch((error) => alert(error.message));
+    };
+    return (
+        <div className="login">
+            <div className="login_container">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt=""/>
+                <div className="login_text">
+                    <h1>Sign in to WhatsApp</h1>
+                </div>
+                <Button onClick={signIn}>
+                    Sign In With Google
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
